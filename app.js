035dfe6a91758c3a62a93bf9ce37dc4ffeb546b7
0 <job.waiswa@gmail.com>
2018-06-13 15:47:59 +0300

    Configuring app.js file for the blog app

--- app.js
+++ app.js
@@ -31,4 +31,15 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model("Blog", blogSchema);
 
+Blog.create({
+    title: "Test Blog",
+    image: "https://images.unsplash.com/photo-1490810277975-e64342ceecf0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=88e69bf894f334456f8ae269752556e1&auto=format&fit=crop&w=200&q=60",
+    body: "HELLO THIS IS A BEVERAGE POST!"
+});
+
+
+
 //RESTFUL  routes
+app.get("/blogs", function(req, res){
+    res.render("index");
+});
