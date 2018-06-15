85569040c70366250f71b75a37160febed83a123
0 <job.waiswa@gmail.com>
2018-06-15 12:48:57 +0300

    Creating RESTFUL  Routes for the blog app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let bodyParser  = require("body-parser"),
+methodOverride  = require("method-override"),
 mongoose        = require("mongoose"),
-express        = require("express"),
-app            = express();
+express         = require("express"),
+app             = express();
+
 
 //Connect the app to mongoDB
 mongoose.connect("mongodb://localhost/blog_app");
@@ -15,11 +17,10 @@ app.use(express.static("public"));
 //set up body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Tell the app to use method-overide
+app.use(methodOverride('_method'));
 
-//Setting app server
-app.listen(3000, function(){
-    console.log("Server is running ..................");
-})
+
 
 //Working on mongoose
 let blogSchema = new mongoose.Schema({
@@ -31,15 +32,88 @@ let blogSchema = new mongoose.Schema({
 
 let Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-    title: "Test Blog",
-    image: "https://images.unsplash.com/photo-1490810277975-e64342ceecf0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=88e69bf894f334456f8ae269752556e1&auto=format&fit=crop&w=200&q=60",
-    body: "HELLO THIS IS A BEVERAGE POST!"
-});
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1490810277975-e64342ceecf0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=88e69bf894f334456f8ae269752556e1&auto=format&fit=crop&w=500&q=60",
+//     body: "HELLO THIS IS A BEVERAGE POST!"
+// });
 
 
 
 //RESTFUL  routes
-app.get("/blogs", function(req, res){
-    res.render("index");
+app.get("/",  function(req, res){
+    res.redirect("/blogs");
 });
+
+//BLOGS ROUTE
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", { blogs: blogs })
+        }
+    });
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    // create a blog in the DB
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW route
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlogs){
+        if(err){
+            res.render("/blogs");
+        }else{
+            res.render("show", {blog: foundBlogs})
+        }
+    });
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlogs){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog: foundBlogs});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs")
+        }else{
+            res.redirect("/blogs/" + req.params.id)
+        }
+    })
+})
+
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    res.send("YOU HAVE REACHED THE DESTROY ROUTE...!!!!")
+});
+
+
+//Setting app server
+app.listen(3000, function () {
+    console.log("Server is running ..................");
+})
